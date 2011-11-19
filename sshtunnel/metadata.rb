e5e01551c5e27959d1e24a34a4c8cefc9fe26c50
Michael Neradkov <michael.neradkov@gmail.com>
2011-11-19 23:05:46 +0400

    add list tunnels

--- sshtunnel/metadata.rb
+++ sshtunnel/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures sshtunnel"
+maintainer_email "michael.neradkov+git@gmail.com"
+license          "All rights reserved" 
+description      "Installs/Configures sshtunnel service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
--- sshtunnel/recipes/default.rb
+++ sshtunnel/recipes/default.rb
@@ -6,3 +6,30 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "sudo" 
+package "python"
+package "python-dev"
+package "python-twisted"
+package "python-pip" 
+package "python-MySQLDb"
+
+
+bash "install_pips" do
+  code <<-RUN_CODE
+  pip install procname simplejson
+  RUN_CODE
+end
+
+git "/var/sshtunnel" do
+  repository "git://github.com/michael-xiii/grid-test.git"
+  reference "master"
+  action :checkout
+end
+
+bash "run service" do
+  code <<-RUN_CODE
+  sudo /var/sshtunnel/SSHTunnelManager/sshtunneld restart
+  RUN_CODE
+end
+
